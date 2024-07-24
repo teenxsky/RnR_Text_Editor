2a4a65b624b52943a85a5757845950c3ae4d62e7
r0manch1k <romansokolv209@gmail.com>
2024-07-24 22:36:42 +0800

    Added font, font size control arrows; Some code fixes

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 import os
 import sys
-import rc.resources
 
 from PySide6.QtCore import QSize, QEvent
-from PySide6.QtGui import QIcon, Qt, QPalette, QColor, QFont, QAction
+from PySide6.QtGui import QIcon, Qt, QColor, QFont
 from PySide6.QtWidgets import QMainWindow, QApplication, QColorDialog, QTextEdit, QMessageBox
 
 from design import Ui_MainWindow
 from service_files import FileManager
-from service_files import setConfigInfo, getConfigInfo
+
+import rc.resources
 
 # RECENT_FILES = []
 
@@ -20,7 +20,7 @@ class TextEditor(Ui_MainWindow, QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.file_manager = FileManager(self.ui.textEdit)
+        self.file_manager = FileManager(self.ui.text_edit)
 
         self._set_icons_svg()
         self._init_setup()
@@ -32,110 +32,78 @@ class TextEditor(Ui_MainWindow, QMainWindow):
         icon_left = QIcon()
         icon_left.addFile(":icons/icons/left.svg", QSize(), QIcon.Normal)
         self.ui.button_left.setIcon(icon_left)
-        self.ui.button_left.setIconSize(QSize(16, 16))
+        self.ui.button_left.setIconSize(QSize(12, 12))
 
         icon_center = QIcon()
         icon_center.addFile(":icons/icons/center.svg", QSize(), QIcon.Normal)
         self.ui.button_center.setIcon(icon_center)
-        self.ui.button_center.setIconSize(QSize(16, 16))
+        self.ui.button_center.setIconSize(QSize(12, 12))
 
         icon_right = QIcon()
         icon_right.addFile(":icons/icons/right.svg", QSize(), QIcon.Normal)
         self.ui.button_right.setIcon(icon_right)
-        self.ui.button_right.setIconSize(QSize(16, 16))
+        self.ui.button_right.setIconSize(QSize(12, 12))
 
         icon_justify = QIcon()
         icon_justify.addFile(":icons/icons/justify.svg", QSize(), QIcon.Normal)
         self.ui.button_justify.setIcon(icon_justify)
-        self.ui.button_justify.setIconSize(QSize(16, 16))
+        self.ui.button_justify.setIconSize(QSize(12, 12))
 
         icon_spacing = QIcon()
         icon_spacing.addFile(":icons/icons/spacing.svg", QSize(), QIcon.Normal)
         self.ui.button_spacing.setIcon(icon_spacing)
-        self.ui.button_spacing.setIconSize(QSize(16, 16))
+        self.ui.button_spacing.setIconSize(QSize(12, 12))
 
     def _init_setup(self):
-        self.ui.actionClose.triggered.connect(self.close)
-        self.ui.actionNew.triggered.connect(self.new_file_event)
-        self.ui.actionOpen.triggered.connect(self.open_file_event)
-        self.ui.actionSave.triggered.connect(self.save_file_event)
-        
-        self.ui.actionCut.triggered.connect(self.ui.textEdit.cut)
-        self.ui.actionCopy.triggered.connect(self.ui.textEdit.copy)
-        self.ui.actionPaste.triggered.connect(self.ui.textEdit.paste)
-        self.ui.actionDelete.triggered.connect(self.delete_event)
-
-        self._default_settings()
-
-        self.ui.textEdit.setAutoFormatting(QTextEdit.AutoFormattingFlag.AutoAll)
-        self.ui.textEdit.selectionChanged.connect(self.update_format)
-
-        self.ui.combo_box_fonts.currentFontChanged.connect(self.ui.textEdit.setCurrentFont)
-        self.ui.spin_box_size.valueChanged.connect(lambda s: self.ui.textEdit.setFontPointSize(float(s)))
-
-        self.ui.button_left.toggled.connect(lambda: self.ui.textEdit.setAlignment(Qt.AlignmentFlag.AlignLeft))
-        self.ui.button_center.toggled.connect(lambda: self.ui.textEdit.setAlignment(Qt.AlignmentFlag.AlignCenter))
-        self.ui.button_right.toggled.connect(lambda: self.ui.textEdit.setAlignment(Qt.AlignmentFlag.AlignRight))
-        self.ui.button_justify.toggled.connect(lambda: self.ui.textEdit.setAlignment(Qt.AlignmentFlag.AlignJustify))
-
-        self.ui.button_bold.toggled.connect(
-            lambda x: self.ui.textEdit.setFontWeight(QFont.Weight.Bold if x else QFont.Weight.Normal))
-        self.ui.button_italic.toggled.connect(lambda x: self.ui.textEdit.setFontItalic(True if x else False))
-        self.ui.button_underline.toggled.connect(lambda x: self.ui.textEdit.setFontUnderline(True if x else False))
-
-        self.update_format()
-        self._update_title()
-        self._update_recent_files()
-    
-    # def _update_recent_files(self):
-
-    #     self.actionRecent_1 = QAction(self)
-    #     self.actionRecent_1.setObjectName(u"actionRecent_1")
-    #     self.menuRecent.addAction(self.actionRecent_1)
-    
-    def delete_event(self):
-        cursor = self.ui.textEdit.textCursor()
-        cursor.removeSelectedText()
-
-    def new_file_event(self):
-        self._default_settings()
-        self._update_title()
-    
-    def open_file_event(self):
-        self.file_manager.open_file()
-        self._update_title()
-    
-    def save_file_event(self):
-        self.file_manager.save_file()
-        self._update_title()
-    
-    def _update_title(self):
-        self.setWindowTitle("%s - R&R Text Editor" % (os.path.basename(self.file_manager.file_path) if self.file_manager.file_path else "Untitled"))
-
-    def _default_settings(self):
-        self.file_manager.file_path = None
-        
-        self.ui.textEdit.clear()
-        self.ui.spin_box_size.setValue(12)
-        self.ui.textEdit.setFontItalic(False)
-        self.ui.textEdit.setFontUnderline(False)
-        self.ui.textEdit.setTextColor(QColor("black"))
-        self.ui.textEdit.setFontWeight(QFont.Weight.Normal)
-        self.ui.combo_box_fonts.setCurrentFont("Times New Roman")
-        self.ui.textEdit.setAlignment(Qt.AlignmentFlag.AlignLeft)
-        self.ui.widget_color.setStyleSheet(f"background-color: {QColor("black").name(QColor.NameFormat.HexRgb)};")
-        self.update_format()
-
-        font = QFont("Times New Roman", 12)
-        self.ui.textEdit.setFont(font)
-        self.ui.textEdit.setFontPointSize(12)
-
-        self.ui.widget_color_picker.setStyleSheet(
-            "QWidget#widget_color_picker{background-color: rgb(244, 244, 244);}")
 
         self.ui.widget_color_picker.installEventFilter(self)
         self.ui.widget_color_picker.setAttribute(Qt.WidgetAttribute.WA_Hover)
         self.ui.widget_color.setAutoFillBackground(True)
+
+        self.ui.text_edit.setAutoFormatting(QTextEdit.AutoFormattingFlag.AutoAll)
+        self.ui.text_edit.selectionChanged.connect(self.update_format)
+
+        self.ui.combo_box_fonts.currentFontChanged.connect(self.ui.text_edit.setCurrentFont)
+        self.ui.spin_box_size.valueChanged.connect(lambda s: self.ui.text_edit.setFontPointSize(float(s)))
+
+        self.ui.button_left.toggled.connect(lambda: self.ui.text_edit.setAlignment(Qt.AlignmentFlag.AlignLeft))
+        self.ui.button_center.toggled.connect(lambda: self.ui.text_edit.setAlignment(Qt.AlignmentFlag.AlignCenter))
+        self.ui.button_right.toggled.connect(lambda: self.ui.text_edit.setAlignment(Qt.AlignmentFlag.AlignRight))
+        self.ui.button_justify.toggled.connect(lambda: self.ui.text_edit.setAlignment(Qt.AlignmentFlag.AlignJustify))
+
+        self.ui.button_bold.toggled.connect(
+            lambda x: self.ui.text_edit.setFontWeight(QFont.Weight.Bold if x else QFont.Weight.Normal))
+        self.ui.button_italic.toggled.connect(lambda x: self.ui.text_edit.setFontItalic(True if x else False))
+        self.ui.button_underline.toggled.connect(lambda x: self.ui.text_edit.setFontUnderline(True if x else False))
+
+        self.ui.action_close.triggered.connect(self.close)
+        self.ui.action_new.triggered.connect(self.new_file_event)
+        self.ui.action_open.triggered.connect(self.open_file_event)
+        self.ui.action_save.triggered.connect(self.save_file_event)
+
+        self.ui.action_cut.triggered.connect(self.ui.text_edit.cut)
+        self.ui.action_copy.triggered.connect(self.ui.text_edit.copy)
+        self.ui.action_paste.triggered.connect(self.ui.text_edit.paste)
+        self.ui.action_delete.triggered.connect(self.delete_event)
+
+        self.ui.spin_box_size.setValue(12)
+        self.ui.text_edit.setFontItalic(False)
+        self.ui.text_edit.setFontUnderline(False)
+        self.ui.text_edit.setTextColor(QColor("black"))
+        self.ui.text_edit.setFontWeight(QFont.Weight.Normal)
+        self.ui.combo_box_fonts.setCurrentFont("Times New Roman")
+        self.ui.text_edit.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        self.set_current_color(QColor('black'))
+
+        font = QFont("Times New Roman", 12)
+        self.ui.text_edit.setFont(font)
+        self.ui.text_edit.setFontPointSize(12)
+
+        self.ui.widget_color_picker.setStyleSheet(
+            "QWidget#widget_color_picker{background-color: rgb(244, 244, 244);}")
+
+        self.update_format()
+        self._update_title()
 
     def eventFilter(self, watched, event):
         if watched == self.ui.widget_color_picker:
@@ -153,6 +121,30 @@ class TextEditor(Ui_MainWindow, QMainWindow):
                 self.color_picker()
 
             return True
+    
+    def delete_event(self):
+        cursor = self.ui.text_edit.textCursor()
+        cursor.removeSelectedText()
+
+    def new_file_event(self):
+        self._default_settings()
+        self._update_title()
+    
+    def open_file_event(self):
+        self.file_manager.open_file()
+        self._update_title()
+    
+    def save_file_event(self):
+        self.file_manager.save_file()
+        self._update_title()
+    
+    def _update_title(self):
+        self.setWindowTitle("%s - R&R Text Editor" % (os.path.basename(self.file_manager.file_path)
+                                                      if self.file_manager.file_path else "Untitled"))
+
+    def _default_settings(self):
+        self.file_manager.file_path = None
+        self.ui.text_edit.clear()
 
     @staticmethod
     def block_signals(objects, b):
@@ -170,26 +162,26 @@ class TextEditor(Ui_MainWindow, QMainWindow):
 
         self.block_signals(signals, True)
 
-        self.ui.combo_box_fonts.setCurrentFont(self.ui.textEdit.currentFont())
-        self.ui.spin_box_size.setValue(int(self.ui.textEdit.fontPointSize()))
+        self.ui.combo_box_fonts.setCurrentFont(self.ui.text_edit.currentFont())
+        self.ui.spin_box_size.setValue(int(self.ui.text_edit.fontPointSize()))
 
-        self.ui.button_bold.setChecked(self.ui.textEdit.fontWeight() == QFont.Weight.Bold)
-        self.ui.button_italic.setChecked(self.ui.textEdit.fontItalic())
-        self.ui.button_underline.setChecked(self.ui.textEdit.fontUnderline())
+        self.ui.button_bold.setChecked(self.ui.text_edit.fontWeight() == QFont.Weight.Bold)
+        self.ui.button_italic.setChecked(self.ui.text_edit.fontItalic())
+        self.ui.button_underline.setChecked(self.ui.text_edit.fontUnderline())
 
-        self.ui.button_left.setChecked(self.ui.textEdit.alignment() == Qt.AlignmentFlag.AlignLeft)
-        self.ui.button_center.setChecked(self.ui.textEdit.alignment() == Qt.AlignmentFlag.AlignCenter)
-        self.ui.button_right.setChecked(self.ui.textEdit.alignment() == Qt.AlignmentFlag.AlignRight)
-        self.ui.button_justify.setChecked(self.ui.textEdit.alignment() == Qt.AlignmentFlag.AlignJustify)
+        self.ui.button_left.setChecked(self.ui.text_edit.alignment() == Qt.AlignmentFlag.AlignLeft)
+        self.ui.button_center.setChecked(self.ui.text_edit.alignment() == Qt.AlignmentFlag.AlignCenter)
+        self.ui.button_right.setChecked(self.ui.text_edit.alignment() == Qt.AlignmentFlag.AlignRight)
+        self.ui.button_justify.setChecked(self.ui.text_edit.alignment() == Qt.AlignmentFlag.AlignJustify)
 
-        self.set_current_color(self.ui.textEdit.textColor())
+        self.set_current_color(self.ui.text_edit.textColor())
 
         self.block_signals(signals, False)
 
     def color_picker(self):
         color = QColorDialog.getColor()
         self.set_current_color(color)
-        self.ui.textEdit.setTextColor(color)
+        self.ui.text_edit.setTextColor(color)
 
     def set_current_color(self, color: QColor):
         self.ui.widget_color.setStyleSheet(f"background-color: {color.name(QColor.NameFormat.HexRgb)};")
@@ -198,12 +190,13 @@ class TextEditor(Ui_MainWindow, QMainWindow):
         reply = QMessageBox.question(self,
                                      'Application Close',
                                      'Do you want to save the current document?',
-                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
+                                     QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No,
+                                     QMessageBox.StandardButton.Yes)
         
-        if reply == QMessageBox.No:
+        if reply == QMessageBox.StandardButton.No:
             event.accept()
         else:
-            self.save_file()
+            # self.save_file()
             event.accept()
 
 
--- src/service_files/file_manager.py
+++ src/service_files/file_manager.py
@@ -1,20 +1,19 @@
 import os
 import subprocess
 
-import mammoth
 import convertapi
+import mammoth
+
+from PySide6.QtWidgets import *
+from PySide6.QtCore import QFile, QDir, QMimeDatabase
+
 convertapi.api_secret = 'Okzibdk295nTMi3e'
 
-from PySide6.QtGui import *
-from PySide6.QtWidgets import *
-from PySide6.QtCore import *
-from PySide6.QtPrintSupport import *
-
-MIME_TYPES = ['application/vnd.openxmlformats-officedocument.wordprocessingml.document', 
-              'application/msword', 
-              'application/pdf', 
-              'text/plain', 
-              'text/html', 
+MIME_TYPES = ['application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+              'application/msword',
+              'application/pdf',
+              'text/plain',
+              'text/html',
               'text/markdown']
 
 
@@ -30,16 +29,17 @@ class FileManager(QMainWindow):
         file_dialog = QFileDialog(self, "Open File")
 
         file_dialog.setMimeTypeFilters(MIME_TYPES)
-        file_dialog.setAcceptMode(QFileDialog.AcceptOpen)
-        file_dialog.setFileMode(QFileDialog.ExistingFile)
+        file_dialog.setAcceptMode(QFileDialog.AcceptMode.AcceptOpen)
+        file_dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
 
-        if file_dialog.exec() != QDialog.Accepted:
+        if file_dialog.exec() != QDialog.DialogCode.Accepted:
             return
         else:
             self.file_path = QDir.toNativeSeparators(file_dialog.selectedFiles()[0])
-        
-        self.mime_type_name = QMimeDatabase().mimeTypeForFileNameAndData(self.file_path, 
-                                                                         QFile(self.file_path).readAll()).name()
+
+        file = QFile(self.file_path)
+        file.open(QFile.OpenModeFlag.ReadOnly)
+        self.mime_type_name = QMimeDatabase().mimeTypeForFileNameAndData(self.file_path, file.readAll()).name()
 
         if self.mime_type_name == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
             with open(self.file_path, 'rb') as docx_file:
@@ -48,50 +48,50 @@ class FileManager(QMainWindow):
 
         elif self.mime_type_name == 'application/msword':
             converted_html_path = self.file_path[:-4] + '.html'
-            convertapi.convert('html', 
-                               {'File': self.file_path}, 
+            convertapi.convert('html',
+                               {'File': self.file_path},
                                from_format='doc').save_files(
-                                   converted_html_path[:-len(converted_html_path.split('/')[-1])])
-            text = open(converted_html_path, 'r', encoding='utf-8').read() 
+                converted_html_path[:-len(converted_html_path.split('/')[-1])])
+            text = open(converted_html_path, 'r', encoding='utf-8').read()
             self.text_widget.setHtml(text)
             os.remove(converted_html_path)
-        
+
         elif self.mime_type_name == 'application/pdf':
             converted_html_path = self.file_path[:-4] + '.html'
-            convertapi.convert('html', 
-                               {'File': self.file_path}, 
+            convertapi.convert('html',
+                               {'File': self.file_path},
                                from_format='pdf').save_files(
-                                   converted_html_path[:-len(converted_html_path.split('/')[-1])])
-            text = open(converted_html_path, 'r', encoding='utf-8').read() 
+                converted_html_path[:-len(converted_html_path.split('/')[-1])])
+            text = open(converted_html_path, 'r', encoding='utf-8').read()
             self.text_widget.setHtml(text)
             os.remove(converted_html_path)
 
         elif self.mime_type_name == 'text/html':
-            text = open(self.file_path, 'r', encoding='utf-8').read() 
+            text = open(self.file_path, 'r', encoding='utf-8').read()
             self.text_widget.setHtml(text)
 
         elif self.mime_type_name == 'text/markdown':
-            text = open(self.file_path, 'r', encoding='utf-8').read() 
+            text = open(self.file_path, 'r', encoding='utf-8').read()
             self.text_widget.setMarkdown(text)
 
         else:
-            text = open(self.file_path, 'r', encoding='utf-8').read() 
+            text = open(self.file_path, 'r', encoding='utf-8').read()
             self.text_widget.setPlainText(text)
-            
+
     def save_file(self):
         file_dialog = QFileDialog(self, "Save File")
 
         file_dialog.setDefaultSuffix("odt")
         file_dialog.setMimeTypeFilters(MIME_TYPES)
-        file_dialog.setAcceptMode(QFileDialog.AcceptSave)
-        file_dialog.setFileMode(QFileDialog.ExistingFile)
+        file_dialog.setAcceptMode(QFileDialog.AcceptMode.AcceptSave)
+        file_dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
         file_dialog.selectFile(self.file_path if self.file_path is not None else "Untitled")
 
-        if file_dialog.exec() != QDialog.Accepted:
+        if file_dialog.exec() != QDialog.DialogCode.Accepted:
             return
         else:
             self.file_path = QDir.toNativeSeparators(file_dialog.selectedFiles()[0])
-        
+
         selected_mime_type = file_dialog.selectedMimeTypeFilter()
 
         if selected_mime_type == 'text/plain':
@@ -110,32 +110,32 @@ class FileManager(QMainWindow):
             elif selected_mime_type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
                 with open(self.file_path[:-5] + '.html', 'w') as file:
                     file.write(text)
-                convertapi.convert('docx', 
-                                   {'File': self.file_path[:-5] + '.html'}, 
-                                   from_format = 'html').save_files(self.file_path)
+                convertapi.convert('docx',
+                                   {'File': self.file_path[:-5] + '.html'},
+                                   from_format='html').save_files(self.file_path)
                 os.remove(self.file_path[:-5] + '.html')
-                
+
             elif selected_mime_type == 'application/msword':
                 with open(self.file_path[:-4] + '.html', 'w') as file:
                     file.write(text)
-                convertapi.convert('docx', 
-                                   {'File': self.file_path[:-4] + '.html'}, 
-                                   from_format = 'html').save_files(self.file_path)
+                convertapi.convert('docx',
+                                   {'File': self.file_path[:-4] + '.html'},
+                                   from_format='html').save_files(self.file_path)
                 os.remove(self.file_path[:-4] + '.html')
-            
+
             elif selected_mime_type == 'application/pdf':
                 with open(self.file_path[:-4] + '.html', 'w') as file:
                     file.write(text)
-                convertapi.convert('pdf', 
-                                   {'File': self.file_path[:-4] + '.html'}, 
-                                   from_format = 'html').save_files(self.file_path)
+                convertapi.convert('pdf',
+                                   {'File': self.file_path[:-4] + '.html'},
+                                   from_format='html').save_files(self.file_path)
                 os.remove(self.file_path[:-4] + '.html')
-        
+
         # subprocess.Popen(['open', self.file_path 
         #                           if os.path.isdir(self.file_path) 
         #                           else os.path.dirname(self.file_path)])
         subprocess.run(["open", "-R", self.file_path])
-            
+
     def _apply_save(self, text: str):
         try:
             with open(self.file_path, 'w') as file:
